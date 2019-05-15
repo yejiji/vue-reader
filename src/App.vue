2ef6413d2941deb43ae08d25516e41c6fbb009c8
Yejj <1271361907@qq.com>
2019-05-15 23:04:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <span class="text">abcd</span>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+  export default {}
+  document.addEventListener('DOMContentLoaded', () => {
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    const html = document.querySelector('html')
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
+<style lang="scss">
+  @import "./assets/styles/global";
+  .text {
+    font-family: 'Days One';
+    font-size: px2rem(20);
+    color:orange;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div id="read">
+        <span class="icon-bookmark"></span>
+        <span class="icon-clock"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Epub from 'epubjs'
+global.ePub = Epub
+export default {
+  name: 'home'
+}
+</script>
