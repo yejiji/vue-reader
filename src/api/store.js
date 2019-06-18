76b0a0b836eb895ad1ffbdabf01cbda86299b7c3
Yejj <1271361907@qq.com>
2019-06-18 21:11:03 +0800

    speaking book

--- src/api/store.js
+++ src/api/store.js
@@ -1,38 +1,45 @@
 import axios from 'axios'
 import {setLocalForage} from '../utils/localForage'
-export function Home() {
-    return axios ({
-        method:'get',
-        url:`http://localhost:8080/book/home `
-    })
+export function flatList() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BOOK_URL}/book/flat-list`
+  })
+}
+
+export function shelf() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/shelf`
+  })
+}
+
+export function home() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/home`
+  })
 }
 
 export function detail(book) {
-    return axios ({
-        method:'get',
-        url:`${process.env.VUE_APP_BOOK_URL}/book/detail`,
-        params: {
-            fileName:book.fileName
-        }
-    })
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BOOK_URL}/book/detail`,
+    params: {
+      fileName: book.fileName
+    }
+  })
 }
 
 export function list() {
-    return axios({
-      method: 'get',
-      url: `${process.env.VUE_APP_BASE_URL}/book/list`
-    })
-  }
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/list`
+  })
+}
 
-export function flatList() {
-    return axios({
-      method: 'get',
-      url: `${process.env.VUE_APP_BOOK_URL}/book/flat-list`
-    })
-  }
-  
-export function download(book, onSuccess,onError,onProgress) {
-  if (!onSuccess) {
+export function download(book, onSucess, onError, onProgress) {
+  if (onProgress == null) {
     onProgress = onError
     onError = null
   }
@@ -41,27 +48,16 @@ export function download(book, onSuccess,onError,onProgress) {
     method: 'get',
     responseType: 'blob',
     timeout: 180 * 1000,
-    onDownloadProgress: ProgressEvent => {
-      if (onProgress) onProgress(ProgressEvent)
+    onDownloadProgress: progressEvent => {
+      if (onProgress) onProgress(progressEvent)
     }
   }).get(`${book.categoryText}/${book.fileName}.epub`)
     .then(res => {
-      const bolb = new Blob([res.data])
-      setLocalForage(book.fileName, bolb, () => {
-        if(onSuccess) onSuccess(book)
-      },err => {
-        if (onError) onError(err)
-      })
+      const blob = new Blob([res.data])
+      setLocalForage(book.fileName, blob,
+        () => onSucess(book),
+        err => onError(err))
     }).catch(err => {
-      if (onError) {
-        onError(err)
-      }
+      if (onError) onError(err)
     })
 }
-
-export function shelf() {
-    return axios({
-      method: 'get',
-      url: `${process.env.VUE_APP_BASE_URL}/book/shelf`
-    })
-  }
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,3 +1,4 @@
+import { getLocalStorage } from './localStorage'
 export const flapCardList = [
   {
     r: 255,
@@ -55,6 +56,39 @@ export const flapCardList = [
     rotateDegree: 0
   }
 ]
+
+export function flatBookList(bookList) {
+  if (bookList) {
+    let orgBookList = bookList.filter(item => {
+      return item.type !== 3
+    })
+    const categoryList = bookList.filter(item => {
+      return item.type === 2
+    })
+    categoryList.forEach(item => {
+      const index = orgBookList.findIndex(v => {
+        return v.id === item.id
+      })
+      if (item.itemList) {
+        item.itemList.forEach(subItem => {
+          orgBookList.splice(index, 0, subItem)
+        })
+      }
+    })
+    orgBookList.forEach((item, index) => {
+      item.id = index + 1
+    })
+    orgBookList = orgBookList.filter(item => item.type !== 2)
+    return orgBookList
+  } else {
+    return []
+  }
+}
+
+export function findBook(fileName) {
+  const bookList = getLocalStorage('shelf')
+  return flatBookList(bookList).find(item => item.fileName === fileName)
+}
 
 export function getCategoryName(id) {
   switch (id) {
